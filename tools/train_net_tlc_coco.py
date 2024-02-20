b24f0295fb69c64facba1245b516ff42cb2eedad
Gudbrand Tandberg <gudbrand@3lc.ai>
2024-02-20 14:33:45 +0100

    Improvements to main script

--- tools/train_net_tlc_coco.py
+++ tools/train_net_tlc_coco.py
@@ -124,22 +124,26 @@ def setup(args):
     """
     Create configs and perform basic setups.
     """
-    cfg = get_cfg()
     from detectron2 import model_zoo
-    cfg.merge_from_file(args.config_file)
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(args.tlc_model_config))
     cfg.merge_from_list(args.opts)
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(args.tlc_model_config)
+    cfg.DATASETS.TRAIN = (args.tlc_train_dataset_name,)
+    cfg.DATASETS.TEST = (args.tlc_val_dataset_name,)
     cfg.freeze()
     default_setup(cfg, args)
     return cfg
 
 
-def setup_mc(trainer: DefaultTrainer, args):
+def setup_metrics_collection(trainer: DefaultTrainer, args):
+    if args.disable_tlc:
+        return
     import tlc
-    from tlc.integration.detectron2 import MetricsCollectionHook # MC-detectron2-faster_rcnn_R_50_FPN_1x
-    tlc.init(project_name=PROJECT_NAME, run_name="VAL-ONLY-TEST")
+    from tlc.integration.detectron2 import MetricsCollectionHook
+    tlc.init(project_name=args.tlc_project_name, run_name=args.tlc_run_name)
 
-    # TRAIN_DATASET_NAME = trainer.cfg.DATASETS.TRAIN[0]
+    TRAIN_DATASET_NAME = trainer.cfg.DATASETS.TRAIN[0]
     VAL_DATASET_NAME = trainer.cfg.DATASETS.TEST[0]
     dataset_metadata = MetadataCatalog.get(VAL_DATASET_NAME)
 
@@ -166,37 +170,50 @@ def setup_mc(trainer: DefaultTrainer, args):
         # ),
     ])
 
-PROJECT_NAME = "COCO-Metrics-Collection"
-COCO_DATASET_ROOT = "C:/Data/coco"
-
 def setup_datasets(cfg, args):
-    # from detectron2.data.datasets import register_coco_instances
-    from tlc.integration.detectron2 import register_coco_instances
-    import tlc
-    try:
-        tlc.register_url_alias("COCO_DATASET_ROOT", f"{COCO_DATASET_ROOT}")
-        tlc.register_url_alias("COCO_TRAIN_2017_IMAGES", f"{COCO_DATASET_ROOT}/train2017")
-        tlc.register_url_alias("COCO_VAL_2017_IMAGES", f"{COCO_DATASET_ROOT}/val2017")
-    except Exception as e:
-        print(e)
-
     TRAIN_DATASET_NAME = cfg.DATASETS.TRAIN[0]
     VAL_DATASET_NAME = cfg.DATASETS.TEST[0]
+    COCO_DATASET_ROOT = args.tlc_coco_dataset_root
 
-    register_coco_instances(
-        TRAIN_DATASET_NAME,
-        {},
-        f"{COCO_DATASET_ROOT}/annotations/instances_train2017.json",
-        f"{COCO_DATASET_ROOT}/train2017",
-        project_name=PROJECT_NAME,
-    )
-    register_coco_instances(
-        VAL_DATASET_NAME,
-        {},
-        f"{COCO_DATASET_ROOT}/annotations/instances_val2017.json",
-        f"{COCO_DATASET_ROOT}/val2017",
-        project_name=PROJECT_NAME,
-    )
+    if args.disable_tlc:
+        from detectron2.data.datasets import register_coco_instances
+        register_coco_instances(
+            name=TRAIN_DATASET_NAME,
+            metadata={},
+            json_file=f"{COCO_DATASET_ROOT}/annotations/instances_train2017.json",
+            image_root=f"{COCO_DATASET_ROOT}/train2017",
+        )
+        register_coco_instances(
+            name=VAL_DATASET_NAME,
+            metadata={},
+            json_file=f"{COCO_DATASET_ROOT}/annotations/instances_val2017.json",
+            image_root=f"{COCO_DATASET_ROOT}/val2017",
+        )
+    else:
+        import tlc
+        from tlc.integration.detectron2 import register_coco_instances
+
+        try:
+            tlc.register_url_alias("COCO_DATASET_ROOT", f"{COCO_DATASET_ROOT}")
+            tlc.register_url_alias("COCO_TRAIN_2017_IMAGES", f"{COCO_DATASET_ROOT}/train2017")
+            tlc.register_url_alias("COCO_VAL_2017_IMAGES", f"{COCO_DATASET_ROOT}/val2017")
+        except Exception as e:
+            print(e)
+
+        register_coco_instances(
+            name=TRAIN_DATASET_NAME,
+            metadata={},
+            json_file=f"{COCO_DATASET_ROOT}/annotations/instances_train2017.json",
+            image_root=f"{COCO_DATASET_ROOT}/train2017",
+            project_name=args.tlc_project_name,
+        )
+        register_coco_instances(
+            name=VAL_DATASET_NAME,
+            metadata={},
+            json_file=f"{COCO_DATASET_ROOT}/annotations/instances_val2017.json",
+            image_root=f"{COCO_DATASET_ROOT}/val2017",
+            project_name=args.tlc_project_name,
+        )
 
 
 
@@ -223,7 +240,7 @@ def main(args):
     
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
-    setup_mc(trainer, args)
+    setup_metrics_collection(trainer, args)
 
     if cfg.TEST.AUG.ENABLED:
         trainer.register_hooks([
@@ -234,7 +251,15 @@ def main(args):
 
 
 def invoke_main() -> None:
-    args = default_argument_parser().parse_args()
+    parser = default_argument_parser()
+    parser.add_argument("--disable-tlc", action="store_true", help="Disable TLC integration")
+    parser.add_argument("--tlc-model-config", type=str, default="COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")
+    parser.add_argument("--tlc-project-name", type=str, default="COCO-Metrics-Collection")
+    parser.add_argument("--tlc-run-name", type=str, default="")
+    parser.add_argument("--tlc-coco-dataset-root", type=str, default="C:/Data/coco")
+    parser.add_argument("--tlc-train-dataset-name", type=str, default="COCO_TRAIN")
+    parser.add_argument("--tlc-val-dataset-name", type=str, default="COCO_VAL")
+    args = parser.parse_args()
     print("Command Line Args:", args)
     launch(
         main,
